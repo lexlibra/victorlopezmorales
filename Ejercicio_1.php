feadb2c1751e91b14f1e59dfbbb0e38ed2714434
lexlibra88 <victorlopez.daw@ciudadescolarfp.es>
2022-02-03 11:28:17 +0100

    inicio

---
+++ Ejercicio_1.php
@@ -0,0 +1,26 @@
+/* 1. Realiza un programa en php que escriba el famoso “Hola Mundo”. El código de php
+deberá de estar embebido dentro de html.*/
+
+<html>
+<head></head>
+
+<body>
+
+<h2>Ejercicio 1/6</h2>
+<h3>Víctor López Morales</h3>
+
+<?php
+
+$saludo="Hola Mundo";
+
+
+//Comillas dobles para las variables"
+
+echo "Hola Mundo";
+
+
+?>
+
+</body>
+
+</html>
